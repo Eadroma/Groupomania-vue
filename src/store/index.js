92af76e9d14759a634e2b4db1135a06541ee7998
Eadroma <boukottaya.jr@gmail.com>
2022-04-25 16:50:56 +0200

    [ADD] basic config for the projects

---
+++ src/store/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import Cookies from 'js-cookie';
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  plugins: [createPersistedState({
+    storage: {
+      getItem: key => Cookies.get(key),
+      setItem: (key, value) => Cookies.set(key, value, {
+        expires: 3,
+        secure: true
+      }),
+      removeItem: key => Cookies.remove(key)
+    }
+  })],
+  state: {
+    user: null,
+    token: null,
+    isLoggedIn: false,
+    loading: false,
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user
+    },
+    setToken(state, token) {
+      state.token = token
+    },
+    setIsLoggedIn(state) {
+      state.isLoggedIn = !state.isLoggedIn
+    }
+  },
+  actions: {
+    setUser({
+      commit
+    }, user) {
+      commit('setUser', user)
+    },
+    setToken({
+      commit
+    }, token) {
+      commit('setToken', token)
+    },
+    setIsLoggedIn({
+      commit
+    }) {
+      commit('setIsLoggedIn')
+    },
+  },
+  getters: {
+    user: state => state.user,
+    token: state => state.token,
+    isLoggedIn: state => state.isLoggedIn,
+
+  }
+})
